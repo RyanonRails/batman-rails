53f28864f74de520785c386bf8b568780fe95dd9
Ryan Jones <ryan@system88.com>
2013-01-12 17:43:31 -0700

    view templates & first pass erb

---
+++ lib/generators/batman/view_generator.rb
@@ -0,0 +1,19 @@
+require 'generators/batman/common'
+module Batman
+  module Generators
+    class ViewGenerator < ::Rails::Generators::NamedBase
+      include Common
+      requires_app_name
+
+      desc "This generator creates Batman views"
+      argument :actions, :type => :array, :default => [], :banner => "action action"
+
+      def create_batman_controller
+        with_app_name do
+          template "views/index.html", "#{js_path}/views/#{plural_name.downcase}/index.html"
+          template "views/_model.html", "#{js_path}/views/#{singular_name.downcase}/_#{singular_name.downcase}.html"
+        end
+      end
+    end
+  end
+end

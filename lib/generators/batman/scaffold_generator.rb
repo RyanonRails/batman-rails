89c04c4c2934f2e8e3d7b3c25cba975bde39737e
Ryan Jones <ryan@system88.com>
2013-01-13 17:41:13 -0700

    add in proper params to view generator

--- lib/generators/batman/scaffold_generator.rb
+++ lib/generators/batman/scaffold_generator.rb
@@ -6,13 +6,14 @@ module Batman
       requires_app_name
 
       desc "This generator creates the client side CRUD scaffolding"
+      argument :attributes, :type => :array, :default => [], :banner => "action action"
 
       def create_batman_model
         with_app_name do
           generate "batman:model #{singular_model_name} --app_name #{app_name}"
           generate "batman:controller #{singular_model_name} index new show create edit update destroy --app_name #{app_name}"
           generate "batman:helper #{singular_model_name}"
-          generate "batman:view #{singular_model_name}"
+          generate "batman:view #{singular_model_name} #{attributes} --app_name #{app_name}""
         end
       end
     end
